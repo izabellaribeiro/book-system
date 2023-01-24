bd9e6e1d33a1d0c313269cfecee11820afbddae4
izabellaribeiro <izabella.ribeirro@gmail.com>
2023-01-24 16:03:40 -0300

    book-system: finalizando as classes de domínio

--- demo/src/main/java/com/example/demo/model/Categoria.java
+++ demo/src/main/java/com/example/demo/model/Categoria.java
@@ -1,24 +1,27 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
 @Entity
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Categoria {
+public class Categoria implements Serializable {
     @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String descricao;
 
-    //private List<Livro> livros = new ArrayList<>();
+    @OneToMany(mappedBy = "categoria")
+    private List<Livro> livros = new ArrayList<>();
 }
--- demo/src/main/java/com/example/demo/model/Livro.java
+++ demo/src/main/java/com/example/demo/model/Livro.java
@@ -1,18 +1,23 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.io.Serializable;
 
 @Entity
 @Setter
 @Getter
-public class Livro {
+public class Livro implements Serializable {
     @Id
     private Long id;
     private String titulo;
     private String nomeAutor;
     private String texto; // aquele que geralmente está presente no livro;
-    //private Categoria categoria;
+
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
 }
